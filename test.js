176767d9f5ab86350ee1450405f7820f43a2b571
mojofill <henryzhang9000@gmail.com>
2022-07-19 18:36:41 +0000

    sdf

--- test.js
+++ test.js
@@ -10,14 +10,16 @@ const WIDTH = document.body.clientWidth;
 const UNIT_WIDTH = 10;
 
 class Rect {
-    constructor(x=0, y=0, scale=1) {
+    constructor(x, y, width, height, scale=1) {
         this.x = x;
         this.y = y;
-        this.width = 50 * scale;
-        this.height = 150 * scale;
+        this.width = width * scale;
+        this.height = height * scale;
         this.heading = 0;
 
         this.scale = scale;
+
+        this.children = [];
     }
 }
 
@@ -41,7 +43,18 @@ canvas2.style.top = `0px`;
 canvas2.style.left = `${WIDTH / 3}px`;
 canvas2.style.position = 'fixed';
 
-let rect = new Rect(200, 200, 1.5);
+let scale = 1.5;
+
+let rect = new Rect(200, 200, 10, 30, scale);
+
+let unit = rect.width;
+
+let rectCenterX = rect.x + rect.width / 2;
+let rectCenterY = rect.y + rect.height / 2;
+
+let food_checker = new Rect(rectCenterX - 2 * unit, rectCenterY - 4 * unit, unit, unit);
+
+rect.children.push(food_checker);
 
 for (let y = 0; y < Math.ceil(canvas2.height / UNIT_WIDTH); y++) {
     gridMap.push([]);
@@ -74,7 +87,7 @@ function pixelate(obj) {
     let leftAngleFlipped = flippedHeading + Math.PI / 2;
     let rightAngleFlipped = flippedHeading - Math.PI / 2;
 
-    for (let radius = 0; radius < obj.width / 2; radius += radiusUnit) {
+    for (let radius = 0; radius < obj.width; radius += radiusUnit) {
         let newCenter = [rectCenterX + radius * Math.cos(obj.heading), rectCenterY + radius * Math.sin(obj.heading)];
         let newCenterFlipped = [rectCenterX + radius * Math.cos(flippedHeading), rectCenterY + radius * Math.sin(flippedHeading)];
 
@@ -121,60 +134,26 @@ function loop() {
     ctx1.fillRect(rect.x, rect.y, rect.width, rect.height);
 
     ctx1.restore();
-    
-    // pixelated version
 
-    let flooredPixels = pixelate(rect);
-    ctx2.fillStyle = 'red';
-    for (const pixel of flooredPixels) {
-        ctx2.fillRect(pixel[0], pixel[1], UNIT_WIDTH, UNIT_WIDTH);
+    ctx1.fillStyle = 'white';
+
+    for (let i = 0; i < rect.children.length; i++) {
+        let obj = rect.children[i];
+        let pixels = pixelate(obj);
+        for (const pixel of pixels) {
+            ctx1.fillRect(pixel[0], pixel[1], UNIT_WIDTH, UNIT_WIDTH);
+        }
     }
 
-    let rectCenterX = rect.x + rect.width / 2;
-    let rectCenterY = rect.y + rect.height / 2;
-
-    let rightAngle = rect.heading - Math.PI / 6 - Math.PI / 2;
-    let leftAngle = rect.heading + Math.PI / 6 - Math.PI / 2;
-
-    ctx2.fillStyle = 'white';
-
-    let leftSquare = new Rect(rectCenterX + 2.5 * rect.width * Math.cos(leftAngle), rectCenterY + rect.width * Math.sin(leftAngle));
-    let rightSquare = new Rect(rectCenterX + 2.5 * rect.width * Math.cos(rightAngle), rectCenterY + rect.width * Math.sin(rightAngle));
-
-    let scale = 10;
-
-    leftSquare.width = scale * UNIT_WIDTH;
-    leftSquare.height = scale * UNIT_WIDTH;
-    rightSquare.width = scale * UNIT_WIDTH;
-    rightSquare.height = scale * UNIT_WIDTH;
-
-    leftSquare.heading = rect.heading;
-    rightSquare.heading = rect.heading;
-
-    leftSquare.x -= leftSquare.width / 2;
-    leftSquare.y -= leftSquare.height / 2;
-
-    let leftPixels = pixelate(leftSquare);
-    let rightPixels = pixelate(rightSquare);
-
-    ctx2.fillRect(leftSquare.x, leftSquare.y, leftSquare.width, leftSquare.height);
-    ctx2.fillRect(rightSquare.x, rightSquare.y, rightSquare.width, rightSquare.height);
-
-    ctx2.fillStyle = 'white';
+    let pixels = pixelate(new Rect(0, 0, 10, 10, 2));
+    for (const pixel of pixels) {
+        ctx1.fillRect(pixel[0], pixel[1], UNIT_WIDTH, UNIT_WIDTH);
+    }
+    // pixelated version
     
-    // for (const pixel of leftPixels) {
-    //     ctx2.fillRect(pixel[0], pixel[1], UNIT_WIDTH, UNIT_WIDTH);
-    // }
-
-    // for (const pixel of rightPixels) {
-    //     ctx2.fillRect(pixel[0], pixel[1], UNIT_WIDTH, UNIT_WIDTH);
-    // }
-    
-    // rect.heading = 69 * Math.PI / 180;
-    rect.heading += 0.01;
     if (rect.heading === 180) rect.heading = 0;
 
-    setTimeout(loop, 1000 / FPS);
+    // setTimeout(loop, 1000 / FPS);
 }
 
 init();
